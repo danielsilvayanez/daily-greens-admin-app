aeae00515343b117ca5c626912ba335047f2c6fc
danielsilvayanez <daniel.silva@xing.com>
2020-12-15 11:41:16 +0100

    delete css files, add header and globalstyles

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,18 +1,22 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
     
     :root{
-        --primaryBg: #3d474d;
-        --primaryFont: #518387;
+        --primaryBgWhite: #FFFFFF;
+        --primaryFontGrey: #474F44;
+        --primaryBGBtnGreen: #1FB760;
+        --secondaryBGPurple: #7F0067;
     }
     
     * {
         box-sizing: border-box;
+        margin: 0;
     }
     
     body {
-        background: var(--primaryBg);
-        color: var(--primaryFont);
+        background: var(--primaryBgWhite);
+        color: var(--primaryFontGrey);
+        font-family: 'Lato', sans-serif;
     }
-`
+`;
