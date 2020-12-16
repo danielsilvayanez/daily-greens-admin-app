8d975b474c08f89221248e8ef4f90155cf6c3a9c
sinafischer <sina.h.fischer@gmail.com>
2020-12-16 17:36:37 +0100

    Add form and basic styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Header } from './components/Header'
-
+import React from 'react';
+import styled from 'styled-components';
+import { Header } from './components/Header';
+import AddDeliveryForm from './pages/AddDeliveryForm';
 
 export default function App() {
   return (
     <AppGrid>
       <Header />
-      
+      <main>
+        <AddDeliveryForm />
+      </main>
     </AppGrid>
-  )
+  );
 }
 
 const AppGrid = styled.div`
   height: 100vh;
   display: grid;
   grid-template-rows: 60px auto 48px;
-`
+`;
---
+++ src/pages/AddDeliveryForm.js
@@ -0,0 +1,151 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+export default function AddDeliveryForm() {
+  const [newDelivery, setNewDelivery] = useState({
+    name: '',
+    street: '',
+    city: '',
+  });
+
+  return (
+    <FormContainer>
+      <h1>Neuer Auftrag</h1>
+      <Form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          name="name"
+          onChange={handleChange}
+          value={newDelivery.name}
+        />
+        <label htmlFor="street">Straße, Hausnummer, Stock:</label>
+        <input
+          type="text"
+          name="street"
+          onChange={handleChange}
+          value={newDelivery.street}
+        />
+        <label htmlFor="city">Stadt:</label>
+        <input
+          type="text"
+          name="city"
+          onChange={handleChange}
+          value={newDelivery.city}
+        />
+        <label htmlFor="postal">Postleitzahl:</label>
+        <input
+          type="text"
+          name="postal"
+          onChange={handleChange}
+          value={newDelivery.postal}
+        />
+        <label htmlFor="dayMeal">Tagesgericht Anzahl:</label>
+        <input
+          type="text"
+          name="dayMeal"
+          onChange={handleChange}
+          value={newDelivery.dayMeal}
+        />
+        <label htmlFor="weekMeal">Wochengericht Anzahl:</label>
+        <input
+          type="text"
+          name="weekMeal"
+          onChange={handleChange}
+          value={newDelivery.weekMeal}
+        />
+        <label htmlFor="item">Weiteres Item:</label>
+        <input
+          type="text"
+          name="item"
+          onChange={handleChange}
+          value={newDelivery.item}
+        />
+        <label htmlFor="date">Lieferdatum:</label>
+        <input
+          type="text"
+          name="date"
+          onChange={handleChange}
+          value={newDelivery.date}
+        />
+        <label htmlFor="driver">Fahrer:</label>
+        <select
+          name="driver"
+          id=""
+          onChange={handleChange}
+          value={newDelivery.driver}
+        >
+          <option value="">Fahrer auswählen</option>
+          <option value="Anton">Anton</option>
+          <option value="Edi">Edi</option>
+          <option value="Det">Det</option>
+        </select>
+        <Button>Erstellen</Button>
+      </Form>
+    </FormContainer>
+  );
+
+  function handleChange(event) {
+    setNewDelivery({
+      ...newDelivery,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setNewDelivery({
+      name: '',
+      street: '',
+      city: '',
+      postal: '',
+      dayMeal: '',
+      weekMeal: '',
+      item: '',
+      date: '',
+      driver: '',
+    });
+  }
+}
+
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 15px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  label {
+    margin-top: 15px;
+  }
+
+  input,
+  select {
+    width: 25rem;
+    padding: 2px;
+    border-radius: 5px;
+    color: var(--primaryFontGrey);
+    font-size: 1rem;
+
+    :focus {
+      outline-color: lime;
+    }
+  }
+`;
+
+const Button = styled.button`
+  background-color: var(--primaryBGBtnGreen);
+  font-family: 'Lato', sans-serif;
+  font-size: 1rem;
+  color: var(--primaryFontGrey);
+  border: none;
+  border-radius: 5px;
+  padding: 4px;
+  width: 5rem;
+  margin: 15px auto 0;
+`;
