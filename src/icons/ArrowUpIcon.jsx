b39bd797d24ab2034f6c1a813cdf3e68dd7abc70
danielsilvayanez <daniel.silva@xing.com>
2020-12-21 10:17:00 +0100

    Modified detiail close logic and add notes area

---
+++ src/icons/ArrowUpIcon.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+function ArrowUpIcon(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 306 306"
+      {...props}
+    >
+      <path d="M35.7 247.35L153 130.05l117.3 117.3 35.7-35.7-153-153-153 153z" />
+    </svg>
+  );
+}
+
+export default ArrowUpIcon;
