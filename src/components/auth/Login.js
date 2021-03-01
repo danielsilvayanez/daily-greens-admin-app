e5101727446deb9a19c59148bd7db909bd581c4b
danielsilvayanez <daniel.silva@xing.com>
2021-03-01 18:01:36 +0100

    add login and logout

---
+++ src/components/auth/Login.js
@@ -0,0 +1,87 @@
+// ./src/components/auth/Login.js
+
+import React, { useRef } from "react";
+import styled from "styled-components";
+import firebaseApp from "../../Firebase/index";
+import { useHistory } from "react-router-dom";
+
+export default function Login() {
+  const userEmail = useRef(null);
+  const userPassword = useRef(null);
+  const history = useHistory();
+
+  async function loginWithFirebase(email, password) {
+    await firebaseApp.signInWithEmailAndPassword(email, password);
+    return history.push("/");
+  }
+
+  return (
+    <Container>
+      <h2>Moin, bitte hier einloggen</h2>
+      <form
+        onSubmit={(event) => (
+          event.preventDefault(),
+          loginWithFirebase(userEmail.current.value, userPassword.current.value)
+        )}
+      >
+        <LoginContainer>
+          <StyledLabel htmlFor="user-email">E-Mail</StyledLabel>
+          <StyledInput
+            htmlId="user-email"
+            name="user-email"
+            type="text"
+            ref={userEmail}
+          />
+
+          <StyledLabel htmlFor="user-password">Password</StyledLabel>
+          <StyledInput
+            htmlId="user-password"
+            name="user-password"
+            type="password"
+            ref={userPassword}
+          />
+
+          <StyledButton type="submit">Login</StyledButton>
+        </LoginContainer>
+      </form>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const LoginContainer = styled.div`
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+  width: 260px;
+`;
+
+const StyledInput = styled.input`
+  padding: 0.5em;
+  border: 1px solid gray;
+  border-radius: 3px;
+  width: 100%;
+  margin-bottom: 10px;
+`;
+
+const StyledLabel = styled.label`
+  display: block;
+`;
+
+const StyledButton = styled.button`
+  display: block;
+  height: 43px;
+  width: 80px;
+  border-radius: 15px;
+  border: none;
+  background-color: var(--secondaryBGPurple);
+  color: var(--primaryBgWhite);
+`;
---
+++ src/components/auth/UserBar.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import LoginContext from "./LoginContext";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
+
+export default function UserBar() {
+  const { user, firebaseApp } = useContext(LoginContext);
+  const history = useHistory();
+
+  async function logoutFromFirebase() {
+    await firebaseApp.signOut();
+    history.push("/");
+  }
+
+  return (
+    <>
+      {user ? <Button onClick={logoutFromFirebase}>Logout</Button> : undefined}
+    </>
+  );
+}
+
+const Button = styled.button`
+  position: absolute;
+  right: 10px;
+  bottom: 55px;
+  border-radius: 50%;
+  border: 1px solid grey;
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px,
+    rgba(0, 0, 0, 0.22) 0px 15px 12px;
+  height: 60px;
+  width: 60px;
+  background-color: var(--primaryBgWhite);
+`;
